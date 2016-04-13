7def0797eb4f8c73d40f9eacc7e43b3cfe0febfd
Chirag Lad <chiragladn92@gmail.com>
2016-04-14 03:18:58 +0530

    ApplicationHooks code added

---
+++ ApplicationHooks/ApplicationHooks/Hooks.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Universal.Music.PlayerControlBoard
+{
+    public interface ControlIF
+    {
+        void PlayIt();
+    }
+
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class AuthorAttribute : System.Attribute
+    {
+        public string DeveloperName { get; set; }
+    }
+}
