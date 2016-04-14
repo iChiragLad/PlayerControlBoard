3d80313758cadcab1b56d17b983f8fcd9b643b0e
Chirag Lad <chiragladn92@gmail.com>
2016-04-14 23:22:00 +0530

    Second Third Party Plugin Added

---
+++ 3rdPartySpeakerSet/3rdPartySpeakerSet/BoseSpeakerJets.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Universal.Music.PlayerControlBoard;
+
+namespace _3rdPartySpeakerSet
+{
+    [Author(DeveloperName = "Bose Inc.")]
+    public class BoseSpeakerJets : ControlIF
+    {
+        public void PlayIt()
+        {
+            Console.WriteLine("Volume Up...Volume down...");
+        }
+        public void Firmware()
+        {
+            Console.WriteLine("Bose v5.1.0.3");
+        }
+    }
+}
