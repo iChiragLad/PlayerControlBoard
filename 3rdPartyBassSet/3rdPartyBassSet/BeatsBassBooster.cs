8d207a8c1fc98809a0055823881ca4595538df12
Chirag Lad <chiragladn92@gmail.com>
2016-04-14 22:58:41 +0530

    First Third Party Plugin Added

---
+++ 3rdPartyBassSet/3rdPartyBassSet/BeatsBassBooster.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Universal.Music.PlayerControlBoard;
+
+namespace _3rdPartyBassSet
+{
+    [Author(DeveloperName = "Beats Inc.")]
+    public class BeatsBassBooster : ControlIF
+    {
+        public void PlayIt()
+        {
+            Console.WriteLine("Beats Beats Bass... Bass... Bum...");
+        }
+        public void Firmware()
+        {
+            Console.WriteLine("Beats v1.0.4.5");
+        }
+    }
+}
